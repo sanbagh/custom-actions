b56b7524292cfb10b2ba3b1745214d3660ff3b55
Sanjeev.Baghel@coxautoinc.com <Sanjeev.Baghel@coxautoinc.com>
2022-09-29 00:50:50 +0530

    create issue action

---
+++ .github/actions/issue/index.js
@@ -0,0 +1,25 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const run = async () => {
+  try {
+    const token = core.getInput("token");
+    const title = core.getInput("title");
+    const body = core.getInput("body");
+    const assignees = core.getInput("assignees");
+
+    const octokit = new github.GitHub(token);
+    const response = await octokit.issue.create({
+      ...github.context.repo,
+      title,
+      body,
+      assignees: assignees ? assignees.split("\n") : undefined,
+    });
+    
+    core.setOutput('issue', JSON.stringify(response.data));
+  } catch (ex) {
+    core.setFailed(ex.message);
+  }
+};
+
+run();
